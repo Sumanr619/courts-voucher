d3cfce39974af15976aa6fab5c9db59bd75e18f5
abhirupanantdv <abhirup.s@anantdv.com>
2026-08-20 10:56:52 +0530

    feat: add voucher program doctype

---
+++ courts_voucher/courts_voucher/doctype/voucher_program/voucher_program.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Suman AnantDV and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Voucher Program", {
+// 	refresh(frm) {
+
+// 	},
+// });
